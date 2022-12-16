32ee0020b55900a63cc70a0766e0df78167dc79b
Mars <marssong66@gmail.com>
2022-12-15 22:02:31 -0800

    Add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import { useAuth } from "./context/authContext";
 import { AuthenticatedApp } from "./authenticatedApp";
 import { UnauthenticatedApp } from "./unauthenticatedApp";
+import { ErrorBoundary } from "./components/errorBoundary";
+import { FullPageErrorFallback } from "./components/lib";
 
 function App() {
   const { user } = useAuth();
 
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
---
+++ src/components/errorBoundary.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  // When child components throws an error, this will receive it and call
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
